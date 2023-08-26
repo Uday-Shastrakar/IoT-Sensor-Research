347037e3be268ed2aa6c96f299fcc5c4de33e214
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-08-26 17:23:25 +0530

    A-1 add code

--- sensor/src/main/kotlin/com/electronic/sensor/api/SensorApi.kt
+++ sensor/src/main/kotlin/com/electronic/sensor/api/SensorApi.kt
@@ -1,6 +1,7 @@
 package com.electronic.sensor.api
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,6 +11,7 @@ import kotlin.collections.HashMap
 
 @RestController
 @RequestMapping("/sensors")
+@CrossOrigin("*")
 class SensorApi {
 
     private val random: Random = Random()
